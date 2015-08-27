04ac7e6b2adaa49ec07d78978f40f413a5086f6b
= <jriedel@paypalcorp.com>
2015-08-27 14:21:32 -0700

    prompt for key added

--- retrieve.py
+++ retrieve.py
@@ -5,12 +5,13 @@ __author__ = 'jriedel'
 from Crypto.Cipher import AES
 from Crypto import Random
 import base64
-import argparse 
+import argparse
+import getpass
 
 # CMD LINE OPTIONS
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--name', action="store", dest="name", required=True, help="the name of the content to retrieve")
-parser.add_argument('-k', '--key', action="store", dest="key", required=True, help="the public key for the encrypted content")
+parser.add_argument('-k', '--key', action="store", dest="key", required=False, help="the public key for the encrypted content")
 args = parser.parse_args()
 
 # CMD LINE VARS
@@ -36,6 +37,8 @@ def retrieve_cipherText(vaultFile):
     return vaultCipherText
 
 if __name__ == '__main__':
+    if not key:
+        key = getpass.getpass('Key:')
 
     BS = 16
     unpad = lambda s : s[:-ord(s[len(s)-1:])]
